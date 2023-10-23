0b5695d05697c9a292e5a4ad8568faa1a6d81d69
alexismaz <alexisfajian@hotmail.com>
2023-10-22 22:44:47 -0300

    Front changes

--- Client/app/Components/Chat.tsx
+++ Client/app/Components/Chat.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Chat() {
   return (
-    <div>Chat</div>
+    <div className='absolute rounded-t-lg bottom-10 right-10 bg-black border-4 border-white h-[30vh] w-[20vw]'>
+        <div className='bg-slate-400 h-[15%]'>
+            Barra de arriba
+        </div>
+        <div className='bg-slate-50 h-[75%] from-gray-700'>
+        </div>
+        <input type="text" placeholder='Hagame una pregunta' title='' className='h-[10%] border-2 border-red-300 w-full'/>
+    </div>
   )
 }
--- Client/app/Home/page.tsx
+++ Client/app/Home/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Chat from '../Components/Chat'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <Chat/>
+    </div>
   )
 }
